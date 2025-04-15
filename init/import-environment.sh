6fac717306b98280e93906cd289c06b78b00e804
Yuri Zamyatin <yuri@yrz.am>
2025-04-15 21:44:14 +0000

    change config format

--- init/import-environment.sh
+++ init/import-environment.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")"
 
-cd "$(dirname "$0")/.."
-[ -d ./themes/yrzam ] && cd ./themes/yrzam
-
-VARS=$(printenv | \
-	grep -E 'THEME_.*' | \
-	sed -e 's/[= ].*//g' | sed -e 's/^/\$/' | tr '\n' ' '
-)
-
-envsubst "$VARS" < "./assets/css/variables.scss.tmpl" > "./assets/css/variables.scss"
+cp ~/.config/theme.json ../data/theme.json
